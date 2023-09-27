944139146a2b28714bb26f5a8bbe2fc6baf1a397
Juan Sebastian Gonzalez Forero <72105720+Sgonzalezf97@users.noreply.github.com>
2023-09-26 22:12:11 -0500

    Firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+     
+      useInheritedMediaQuery: true,
+      title: 'Application',
+      initialRoute:Routes.HOME,
       getPages: AppPages.routes,
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+     
+    );
+  }
 }
